d4a439fd1015d11feb5dd09721d1ce5b938355b9
Lucas Yohan <lucalen.y@live.com>
2026-09-22 15:48:16 -0300

    feat(autor): adicionado autor model

---
+++ src/main/java/com/lyalencar/demo_spring_rev_jpa/model/Autor.java
@@ -0,0 +1,47 @@
+package com.lyalencar.demo_spring_rev_jpa.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "autores")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter @Setter
+public class Autor implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_autor", nullable = false)
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 50)
+    private String nome;
+
+    @Column(name = "sobrenome", nullable = false, length = 50)
+    private String sobrenome;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Autor autor = (Autor) o;
+        return Objects.equals(id, autor.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Autor{" +
+                "id=" + id +
+                '}';
+    }
+}
