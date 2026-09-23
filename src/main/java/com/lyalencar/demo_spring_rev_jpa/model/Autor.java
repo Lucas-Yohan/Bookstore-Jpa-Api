451ff93a0809073f7fcb81995344007a46c3356a
Lucas Yohan <lucalen.y@live.com>
2026-09-23 18:47:55 -0300

    fix(Autor): adicionado jpa persist em infoAutor

--- src/main/java/com/lyalencar/demo_spring_rev_jpa/model/Autor.java
+++ src/main/java/com/lyalencar/demo_spring_rev_jpa/model/Autor.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.apache.logging.log4j.util.Lazy;
 
 import java.io.Serializable;
 import java.util.Objects;
@@ -26,6 +27,11 @@ public class Autor implements Serializable {
     @Column(name = "sobrenome", nullable = false, length = 50)
     private String sobrenome;
 
+    @OneToOne(cascade = {CascadeType.PERSIST})
+    @JoinColumn(name = "info_autor_id_info")
+    private InfoAutor infoAutor;
+
+    // EQUALS HASCODE
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
