676827d920a04d4579a11ad3113cadc07c293eb5
Lucas Yohan <lucalen.y@live.com>
2026-09-23 18:49:00 -0300

    feat(InfoAutor): adicionado camada repo

---
+++ src/main/java/com/lyalencar/demo_spring_rev_jpa/repository/InfoAutorRepository.java
@@ -0,0 +1,8 @@
+package com.lyalencar.demo_spring_rev_jpa.repository;
+
+import com.lyalencar.demo_spring_rev_jpa.model.InfoAutor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface InfoAutorRepository extends JpaRepository<InfoAutor, Long> {
+}
